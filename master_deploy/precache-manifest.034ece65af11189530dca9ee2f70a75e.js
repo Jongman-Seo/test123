836ec6aef3cfa474ba19b45e8799fce835056ab6
Jongman-Seo <jongman@dable.io>
2020-10-07 00:06:37 +0900

    deploy

---
+++ master_deploy/precache-manifest.034ece65af11189530dca9ee2f70a75e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "984c90c5c6b743e5c5a8fc8e86cbca69",
+    "url": "/test123/master_deploy/index.html"
+  },
+  {
+    "revision": "19de0fff3a70c8aedf7d",
+    "url": "/test123/master_deploy/static/js/2.e6d7804a.chunk.js"
+  },
+  {
+    "revision": "d2a181f44fad8adfc745616a61720c25",
+    "url": "/test123/master_deploy/static/js/2.e6d7804a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4789f354e110c496f3ff",
+    "url": "/test123/master_deploy/static/js/main.6aef6b6e.chunk.js"
+  },
+  {
+    "revision": "5aa07b6b2c542a1ce4b3",
+    "url": "/test123/master_deploy/static/js/runtime-main.d41605d6.js"
+  }
+]);
