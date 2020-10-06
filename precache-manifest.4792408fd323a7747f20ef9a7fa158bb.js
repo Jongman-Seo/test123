1159eb82b1dcfa5f1e8eafa87b012ba693b145b1
Jongman-Seo <jongman@dable.io>
2020-10-06 18:51:46 +0900

    Updates

---
+++ precache-manifest.4792408fd323a7747f20ef9a7fa158bb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1159f81dd34f521a4c9476b09c82bddd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "19de0fff3a70c8aedf7d",
+    "url": "/static/js/2.e6d7804a.chunk.js"
+  },
+  {
+    "revision": "d2a181f44fad8adfc745616a61720c25",
+    "url": "/static/js/2.e6d7804a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e3d73c9832ae37ceff45",
+    "url": "/static/js/main.c5fbeaa7.chunk.js"
+  },
+  {
+    "revision": "14b088db8697c9f21c26",
+    "url": "/static/js/runtime-main.b0ac5ca1.js"
+  }
+]);
